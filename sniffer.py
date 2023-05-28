0e4faa157eb64ce9860b56f282561ce084872e1f
Rutik <rutiksarode94@gmail.com>
2023-05-28 12:24:08 +0530

    Done

--- sniffer.py
+++ sniffer.py
@@ -1,12 +1,8 @@
-# -*- coding: utf-8 -*-
-&quot;&quot;&quot;
-Created on Wed Feb 15 11:51:01 2023
-@author: exam
-&quot;&quot;&quot;
+
 from scapy.all import *
 def handler(packet):
-print(packet.summary())
-sniff(iface=&quot;wlp1s0&quot;, prn=handler, store=0)
+  print(packet.summary())
+sniff(iface="wlp1s0", prn=handler, store=0)
 # Run with following command
 # sudo python3 sniffer.py
 # You can change last network id -&gt; wlp1s0as
